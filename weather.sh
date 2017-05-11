4517dc036eb0cc2bf34c10e6db9146dff871e5d4
Douglas Leeder <douglas.leeder@gmail.com>
2017-05-11 21:15:34 +0100

    Disable my post processing steps.

--- weather.sh
+++ weather.sh
@@ -4,16 +4,16 @@ cd ~
 # python -m pywws.Hourly ~/weather/data
 pywws-hourly -vvv ~/weather/data
 
-function txt2html()
-{
-(
-        echo "<html><title>$1</title><body>"
-        cat webdata/$1.txt
-        echo "</body></html>"
-) >webdata/$1.html
-}
+#function txt2html()
+#{
+#(
+#        echo "<html><title>$1</title><body>"
+#        cat webdata/$1.txt
+#        echo "</body></html>"
+#) >webdata/$1.html
+#}
 
-txt2html 24hrs
-txt2html 6hrs
-txt2html 7days
-txt2html allmonths
+#txt2html 24hrs
+#txt2html 6hrs
+#txt2html 7days
+#txt2html allmonths
